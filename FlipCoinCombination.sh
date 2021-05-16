ea022364f6fe216d0f3a95fc1fc914096c9bf57b
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-05-16 23:29:17 +0530

    Added: doublate combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -5,17 +5,23 @@ declare  -A  compute
 for i in `seq 10`
 do
 choice=$((RANDOM%2))
-
+count=0
+for((count=0; i<count; count++))
+do
 if [ $choice -eq 1 ]
 then 
 	echo result= "head"
+		count += 1
 elif [ $choice -eq 0 ]
 then
 	echo result1= "tail"
-fi 
+   count += 1
+fi
+done 
 done
 
-xcompute["result"]=$choice
+compute["result"]=$choice
 compute["result1"]=$choice
 echo descending order : ${compute[@]}
-
+percentage=$(($choice/$i%2))
+echo $percentage
