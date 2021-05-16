668623d6e8c1a394e036ec2e2ce655036ea546ac
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-05-16 20:35:13 +0530

    Added: singlet of coins

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -x
 
+
+declare  -A  compute
+for i in `seq 10`
+do
 choice=$((RANDOM%2))
+
 if [ $choice -eq 1 ]
-then
-   echo "head"
+then 
+	echo result= "head"
 elif [ $choice -eq 0 ]
 then
-   echo "tail"
-fi
+	echo result1= "tail"
+fi 
+done
+
+xcompute["result"]=$choice
+compute["result1"]=$choice
+echo descending order : ${compute[@]}
+
